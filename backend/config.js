0bb6f6124d24e466353ea5fe62a4716e5790837d
Aswathy-tech <123970842+Aswathy-tech@users.noreply.github.com>
2024-06-03 15:23:29 +0200

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
-export const PORT = 3000;
+export const PORT = process.env.PORT || 3000;
 
 export const mongoDBURL =
 'mongodb+srv://root:root4@bookstore-mern.av1mpo8.mongodb.net/?retryWrites=true&w=majority';
