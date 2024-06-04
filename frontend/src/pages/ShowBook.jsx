971886280291ad2840c50a8009224fb479b0a416
Aswathy-tech <123970842+Aswathy-tech@users.noreply.github.com>
2024-06-04 13:21:10 +0200

    Update ShowBook.jsx

--- frontend/src/pages/ShowBook.jsx
+++ frontend/src/pages/ShowBook.jsx
@@ -12,7 +12,7 @@ const ShowBook = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get(`https://deploy-fullstack-bookstore-api.vercel.app/api/${id}`)
+      .get(`https://deploy-fullstack-bookstore-api.vercel.app/books/${id}`)
       .then((response) => {
         setBook(response.data);
         setLoading(false);
