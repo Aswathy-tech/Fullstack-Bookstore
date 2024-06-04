d40cd5462023b176a9ce4d8fe102d1c51686bf93
Aswathy-tech <123970842+Aswathy-tech@users.noreply.github.com>
2024-06-04 13:20:54 +0200

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get('https://deploy-fullstack-bookstore-api.vercel.app/api')
+      .get('https://deploy-fullstack-bookstore-api.vercel.app/books')
       .then((response) => {
         setBooks(response.data.data);
         setLoading(false);
