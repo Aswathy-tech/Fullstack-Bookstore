138d10e4d50623d4c7551989ca587c360e14f6e9
Aswathy-tech <123970842+Aswathy-tech@users.noreply.github.com>
2024-06-04 13:11:37 +0200

    Update EditBook.jsx

--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -16,7 +16,7 @@ const EditBook = () => {
 
   useEffect(() => {
     setLoading(true);
-    axios.get(`https://deploy-fullstack-bookstore-api.vercel.app/books/${id}`)
+    axios.get(`https://deploy-fullstack-bookstore-api.vercel.app/api/${id}`)
     .then((response) => {
         setAuthor(response.data.author);
         setPublishYear(response.data.publishYear)
